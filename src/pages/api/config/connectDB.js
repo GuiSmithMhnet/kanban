9bde03221e4915cf877e0cd175d5d85cc5b64d40
GuiSmithMhnet <guilherme.rodrigues@mhnet.com.br>
2026-05-07 17:49:57 -0300

    chore: conexão BD alterada para local teste

--- src/pages/api/config/connectDB.js
+++ src/pages/api/config/connectDB.js
@@ -1,5 +1,6 @@
 import { Pool } from 'pg';
 
+// Prod
 const pool = new Pool({
     host: process.env.POSTGRES_HOST,
     user: process.env.POSTGRES_USER,
@@ -8,5 +9,15 @@ const pool = new Pool({
     database: process.env.POSTGRES_DB,
     ssl: false
 });
+
+// Dev
+// const pool = new Pool({
+//     host: 'localhost',
+//     user: process.env.POSTGRES_USER,
+//     port: 5433,
+//     password: process.env.POSTGRES_PASSWORD,
+//     database: process.env.POSTGRES_DB,
+//     ssl: false
+// });
 
 export default pool;
