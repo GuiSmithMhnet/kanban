7a0715dde4e992e7bc7aac18311aeac967c97ee2
GuiSmithMhnet <guilherme.rodrigues@mhnet.com.br>
2026-05-15 14:38:44 -0300

    chore(env): parameterize app ports and current base url

--- src/pages/api/config/getCurrentUrl.js
+++ src/pages/api/config/getCurrentUrl.js
@@ -1,10 +1,10 @@
 const getCurrentUrl = () => {
     if(process.env.STAGE == 'production'){
-        return 'http://localhost';
+        return `http://localhost:${process.env.PRODUCTION_PORT}`;
     }
 
     if(process.env.STAGE == 'development'){
-        return 'http://localhost';
+        return `http://localhost:${process.env.DEVELOPMENT_PORT}`;
     }
 }
 
